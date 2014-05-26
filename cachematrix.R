f7a1c7cc5e6320e9b635e256e583e72e39b7383f
bezoarboy <bezoarboy@users.noreply.github.com>
2014-05-25 21:13:00 -0400

    Clarify comments, grouped with functions

--- cachematrix.R
+++ cachematrix.R
@@ -4,25 +4,19 @@
 ## - pair of functions that cache the inverse of a matrix
 ## - v1.0, 2014-05-17
 ##
-## <<- operator assigns a value to an object in an environment  different from the current environment
-##
-## makeCacheMatrix
-## - creates a special "vector", consisting of a list containing functions to
-##  - set the value of the matrix
-##  - get the value of the matrix
-##  - set the value of the inverse
-##  - get the value of the inverse
-##
-## cacheSolve
-## - calculates the inverse of the special "vector" created with makeCacheMatrix
-## - first checks to see if the inverse has already been calculated
-## - if so, it gets the inverse from the cache and skips the computation
-## - otherwise, it calculates the inverse of the data and caches it via the setInverse function
+## <<- operator assigns a value to an object in an environment different from the current environment
+##  - can be thought of as a global variable
 ##
 
 ##########
 ## makeCacheMatrix
 ## - creates a special "matrix" object that can cache its inverse
+## - consists of a list containing functions to
+##  - set the value of the matrix
+##  - get the value of the matrix
+##  - set the value of the inverse
+##  - get the value of the inverse
+##
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
     set <- function(y) {
@@ -38,9 +32,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ##########
 ## cacheSolve
-## - return a matrix that is the inverse of 'x'
 ## - computes the inverse of the special "matrix" returned by makeCacheMatrix and cache it
+## - first checks to see if the inverse has already been calculated
 ## - if inverse already calculated (and matrix unchanged), then retrieve the inverse from the cache
+## - otherwise, it calculates the inverse of the data and caches it via the setInverse function
+##
 cacheSolve <- function(x, ...) {
     i <- x$getInverse()
     if(!is.null(i)) {
